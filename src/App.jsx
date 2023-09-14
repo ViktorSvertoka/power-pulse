2f669f7f76d3822f1565816bed33d247935f647b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-09-14 19:29:03 +0300

    Feat(React) Added files to new project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import FirstPage from './pages/FirstPage/FirstPage';
+import SecondPage from './pages/SecondPage/SecondPage';
+import HalfPage from './pages/HalfPage/HalfPage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+
+const test = import.meta.env.VITE_API_TEST;
+
+function App() {
+  console.log(test);
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/first" element={<FirstPage />} />
+        <Route path="/second" element={<SecondPage />}>
+          <Route path=":half" element={<HalfPage />} />
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+}
+export default App;
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './ErrorPage.styled';
+
+const ErrorPage = () => {
+  return (
+    <Container>
+      <Title>404</Title>
+    </Container>
+  );
+};
+
+export default ErrorPage;
