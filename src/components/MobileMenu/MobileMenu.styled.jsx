c115fd89bb28d77f2056c9b42789719e66618a07
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-09-16 00:15:27 +0300

    Style(CSS) Code style edits to mobile menu

--- src/components/MobileMenu/MobileMenu.styled.jsx
+++ src/components/MobileMenu/MobileMenu.styled.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const MenuWrapper = styled.div`
+  background-color: var(--orange-color);
   display: flex;
   flex-direction: column;
   height: 100vh;
