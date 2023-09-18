e8b1e271c11089381f602fb2235bae20bc941fa1
Serhii Yevchishen <magodza06@gmail.com>
2023-09-18 15:02:31 +0300

    Style(SCSS) Code style edits

--- src/pages/SignUpPage/RegistrationPageStyled.jsx
+++ src/pages/SignUpPage/RegistrationPageStyled.jsx
@@ -1,36 +1,45 @@
 import styled from 'styled-components';
-// import { Field } from 'formik';
-import backgroundMob from 'src/images/hero-1x.jpg';
-
+import backgroundMob from 'src/images/side-view.jpg';
+import background from 'src/images/hero-2x.jpg';
+import backgroundTabl from 'src/images/side-tabl.jpg';
 export const Container = styled.div`
   display: flex;
   max-width: 100vw;
-  /* max-width: 1440px; */
+
   height: 100vh;
-  background-image: url(${backgroundMob});
-  /* background-size: 50% 100%; */
-  /* background-position: 50% right; */
+  /* background-image: url(${backgroundMob}); */
+  background:
+    linear-gradient(168deg, #040404 14.75%, rgba(4, 4, 4, 0) 52.97%),
+    linear-gradient(73deg, #040404 6.11%, rgba(4, 4, 4, 0) 66.68%),
+    url(${backgroundMob});
+   
   background-position: right;
   background-repeat: no-repeat;
-  background-size: 50% 100%;
-  /* @media screen and (min-width: 375px) {
-    /* max-width: 100vw;
-    
-    max-width: 100vw; */
-    /* background-size: 50% 100%; */
-    
-   
-  /*
+  background-size: 90% 100%;
+  background-position-y: calc(100% + 250px);
+
+  @media screen and (min-width: 375px) {
+    width: 100vw;
+    background-image: url(${backgroundMob});
+    background-size: 70% 100%;
+
+    background-position-y: calc(100% + 170px);
+  }
+
   @media screen and (min-width: 768px) {
-    max-width: 100vw;
-    padding: 60px 75px;
-    border-radius: 40px;
-    gap: 22px;
+    width: 100vw;
+    background-image: url(${backgroundTabl});
+    background-size: 50% 100%;
+
+    background-position-y: calc(100% + 60px);
   }
 
   @media screen and (min-width: 1440px) {
-    gap: 8px;
-  } */
+    width:100%;
+    background-size: 50% 100%;
+    background-image: url(${background});
+    background-position-y: calc(100% + 120px);
+  }
 `;
 
 
