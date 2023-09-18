ada5b098d51b971d1c4cd69151ca0e2dc9a90775
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-09-18 11:15:28 +0300

    Created component UserProfile

---
+++ src/components/UserProfile/UserProfile.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Avatar = styled.div`
+  width: 90px;
+  height: 90px;
+  border-radius: 100px;
+  border: 1px solid #e6533c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,37 +1,15 @@
-import { Container, Title, Avatar } from './UserPage.styled';
+import { Container, Title } from './UserPage.styled';
 import DashboardItem from '../../components/DashboardItem/DashboardItem';
 import ExclamationMark from '../../components/ExclamationMark/ExclamationMark';
 import UserForm from '../../components/UserForm/UserForm';
 import Logout from '../../components/Logout/Logout';
+import UserProfile from '../../components/UserProfile/UserProfile';
 
 const UserPage = () => {
   return (
     <Container>
       <Title>Profile Settings</Title>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          marginTop: '40px',
-        }}
-      >
-        <Avatar>
-          <svg fill="var(--normal-color)" width="62" height="62">
-            <use href="./src/images/sprite.svg#icon-user"></use>{' '}
-          </svg>
-        </Avatar>
-        <svg
-          style={{ position: 'relative', top: '-13' }}
-          width="24"
-          height="24"
-        >
-          <use href="./src/images/sprite.svg#icon-add-avatar"></use>{' '}
-        </svg>
-        <p style={{ color: '#EFEDE8' }}>Anna Rybachok</p>
-        <p style={{ color: 'var(--normal-color)' }}>User</p>
-      </div>
+      <UserProfile />
       <div
         style={{
           display: 'flex',
