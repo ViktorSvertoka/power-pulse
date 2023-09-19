e2b31f56245dc03649cc5c9210d1a24ad58ee63d
Dmitriy Boiko <dmitriy1boiko@gmail.com>
2023-09-19 22:14:11 +0300

    proucts(css) finally

---
+++ src/components/products/SearchNotResult/SearchNotResult.styles.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const NotResultWrapper = styled.div`
+ @media screen and (min-width: 375px) {
+    display: inline-block;
+    margin-top: 24px;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 92px;
+  }
+`;
+
+export const NotResultText = styled.p`
+color: rgba(239, 237, 232, 0.3);
+@media screen and (min-width: 375px) {
+  
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 400;
+    font-style: normal;
+    display: inline-block;
+    width: 335px;
+    margin-bottom: 16px;
+  }
+ 
+    @media screen and (min-width: 768px) {
+      width: 580px;
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+
+export const NotResultOrangeText = styled.span`
+color: var(--orange-color);
+@media screen and (min-width: 375px) {
+  
+    font-size: 14px;
+    line-height: 18px;
+    display: inline-block;
+  }
+ 
+    @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+
+export const NotResultOrangeTextBlock = styled.p`
+color: var(--orange-color);
+@media screen and (min-width: 375px) {
+  
+    font-size: 14px;
+    line-height: 18px;
+    display: inline-block;
+  }
+ 
+    @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 24px;
+    display: block;
+  }
+`;
