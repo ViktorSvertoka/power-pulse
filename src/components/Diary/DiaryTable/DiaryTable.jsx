e72b60aefa8cf1bc6c09ef3ed267844f8268e7ee
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-09-19 08:39:31 +0200

    Fix (table)

--- src/components/Diary/DiaryTable/DiaryTable.jsx
+++ src/components/Diary/DiaryTable/DiaryTable.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import {
-  Heading,
   RecommendCell,
   RecommendText,
   RecommendWrap,
@@ -8,7 +6,8 @@ import {
   Table,
   Tbody,
   Td,
-  TdHead,
+  Thead,
+  Th,
   Tr,
 } from './DiaryTable.styled';
 
@@ -21,20 +20,22 @@ const DiaryTable = () => {
 
   return (
     <Table>
-      <Tbody>
+      <Thead>
         <Tr>
           {headings.map((heading, index) => (
-            <TdHead key={index}>
-              <Heading>{heading}</Heading>
-            </TdHead>
+            <Th key={index} index={index}>
+              {heading}
+            </Th>
           ))}
         </Tr>
+      </Thead>
+      <Tbody>
         {rows.map((row, rowIndex) => (
           <Tr key={rowIndex}>
             {cells.map((cell, cellIndex) => {
               if (cellIndex === 4) {
                 return (
-                  <Td key={cellIndex}>
+                  <Td key={cellIndex} index={cellIndex}>
                     <RecommendWrap>
                       <RecommendCell></RecommendCell>
                       <RecommendText>Yes</RecommendText>
@@ -44,14 +45,36 @@ const DiaryTable = () => {
               }
               if (cellIndex === 5) {
                 return (
-                  <Td key={cellIndex} style={{ border: 'none' }}>
-                    <Svg>
+                  <Td
+                    key={cellIndex}
+                    style={{
+                      border: 'none',
+                      textOverflow: 'none',
+                      whiteSpace: 'nowrap',
+                      padding: '0',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                    index={cellIndex}
+                  >
+                    <Svg index={cellIndex}>
                       <use href={`${sprite}#icon-trash`} />
                     </Svg>
                   </Td>
                 );
               }
-              return <Td key={cellIndex}>Произвольный текст</Td>;
+
+              return (
+                <Td
+                  key={cellIndex}
+                  style={{
+                    textOverflow: 'ellipsis',
+                  }}
+                  index={cellIndex}
+                >
+                  Произвольный текст
+                </Td>
+              );
             })}
           </Tr>
         ))}
