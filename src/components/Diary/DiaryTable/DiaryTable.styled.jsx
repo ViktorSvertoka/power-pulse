82948f3f4c86bce87b003390b9d7a8727a06d24e
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-09-19 08:45:59 +0200

    Fix (Table) end

--- src/components/Diary/DiaryTable/DiaryTable.styled.jsx
+++ src/components/Diary/DiaryTable/DiaryTable.styled.jsx
@@ -2,11 +2,19 @@ import styled from 'styled-components';
 
 const Table = styled.table`
   display: flex;
+  flex-direction: column;
   max-height: 164px;
   max-width: 100%;
   overflow-x: hidden;
   margin: 16px;
   border-radius: 12px;
+`;
+
+const Thead = styled.thead`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 8px;
 `;
 
 const Tbody = styled.tbody`
@@ -21,27 +29,48 @@ const Tr = styled.tr`
   display: flex;
   align-items: center;
   gap: 8px;
-  color: #efede8;
 `;
 
-const Td = styled.td`
-  height: 40px;
+const Th = styled.th`
+  flex: 0 0 auto;
+  width: ${props => {
+    if (props.index === 0) return '212px';
+    if (props.index === 1) return '166px';
+    if (props.index === 2 || props.index === 3) return '105px';
+    if (props.index === 4) return '110px';
+    return 'auto';
+  }};
+  display: flex;
   align-items: center;
-  border-radius: 12px;
-  padding: 8px 14px;
-  border: 1px solid rgba(239, 237, 232, 0.3);
-  color: #efede8;
-  white-space: nowrap;
-
-  overflow: hidden;
+  gap: 8px;
+  color: #ef8964;
   font-family: 'RobotoRegular';
-  font-size: 14px;
-  font-weight: 400;
+  font-size: 12px;
   line-height: 1.5;
 `;
 
-const TdHead = styled.td`
-  width: 100%;
+const Td = styled.td`
+  flex: 0 0 auto;
+  width: ${props => {
+    if (props.index === 0) return '212px';
+    if (props.index === 1) return '166px';
+    if (props.index === 2 || props.index === 3) return '105px';
+    if (props.index === 4) return '110px';
+    return 'auto';
+  }};
+  height: ${props => (props.index === 5 ? '40px' : 'auto')};
+  align-items: center;
+  border-radius: 12px;
+  padding: 8px 14px;
+  border: ${props =>
+    props.index === 5 ? 'none' : '1px solid rgba(239, 237, 232, 0.3)'};
+  color: #efede8;
+  white-space: nowrap;
+  overflow: hidden;
+  font-family: 'RobotoRegular';
+  font-size: ${props => (props.index === 4 ? '16px' : '14px')};
+  font-weight: ${props => (props.index === 4 ? '700' : '400')};
+  line-height: 1.5;
 `;
 
 const Heading = styled.div`
@@ -76,19 +105,20 @@ const RecommendText = styled.span`
 `;
 
 const Svg = styled.svg`
-  width: 20px;
-  height: 20px;
+  width: ${props => (props.index === 5 ? '20px' : 'auto')};
+  height: ${props => (props.index === 5 ? '20px' : 'auto')};
   fill: #ffffff;
   stroke: #ffffff;
 `;
 
 export {
   Table,
+  Thead,
   Tbody,
   Tr,
+  Th,
   Td,
   Heading,
-  TdHead,
   RecommendCell,
   RecommendText,
   RecommendWrap,
