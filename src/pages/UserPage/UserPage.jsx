61f779acbc74036c002c0c19df969db97edd6a8c
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-09-21 18:46:48 +0300

    Update UserPage.jsx

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,18 +1,27 @@
 import { Container, Title, Wrapper } from './UserPage.styled';
-import DashboardItem from '../../components/DashboardItem/DashboardItem';
 import ExclamationMark from '../../components/ExclamationMark/ExclamationMark';
 import UserForm from '../../components/UserForm/UserForm';
 import Logout from '../../components/Logout/Logout';
 import UserProfile from '../../components/UserProfile/UserProfile';
+import DashboardItem from '../../components/DashboardItem/DashboardItem';
 
 const UserPage = () => {
+  const icons = ['icon-fork-knife', 'icon-dumbbell'];
   return (
     <Container>
       <Title>Profile Settings</Title>
       <UserProfile />
       <Wrapper>
-        <DashboardItem color="var(--orange-color)" iconId="icon-fork-knife" />
-        <DashboardItem color="var(--orange-color)" iconId="icon-dumbbell" />
+        <DashboardItem
+          color="var(--orange-color)"
+          iconId="icon-fork-knife"
+          icons={icons}
+        />
+        <DashboardItem
+          color="var(--orange-color)"
+          iconId="icon-dumbbell"
+          icons={icons}
+        />
       </Wrapper>
       <ExclamationMark />
       <Logout />
