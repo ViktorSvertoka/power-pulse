909e3ebbe5466391e3502671bc8e70b36943f424
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-09-21 17:59:08 +0300

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-  max-width: 1440px;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -11,12 +11,18 @@ export const HeaderContainer = styled.div`
 
   background-color: var('--black-color');
 
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
+
   @media screen and (min-width: 768px) {
     padding: 32px 32px;
+    width: 768px;
   }
 
   @media screen and (min-width: 1440px) {
     padding: 32px 96px;
+    width: 1440px;
   }
 `;
 
