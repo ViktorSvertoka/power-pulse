870e1896807f76a28aacdc063bd4fb5e6b0f8395
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-09-22 22:18:18 +0300

    UserProfile edited
    UserProfile edited

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+
 import sprite from '../../images/sprite.svg';
-import { Button, Avatar, Wrapper, IconBtn } from './UserProfile.styled';
+import { Button, Avatar, Photo, Wrapper, IconBtn } from './UserProfile.styled';
 
 const UserProfile = () => {
+  const [avatarUrl, setAvatarUrl] = useState(
+    'https://images.squarespace-cdn.com/content/v1/58d1b3ff1b631bb1893d108d/813f4928-6cc6-4bc8-a4e4-265f94b4d665/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg?format=1500w',
+  );
+
+  const avatarUser = <Photo src={avatarUrl} width="100%" alt="Avatar" />;
+  const avatarLogo = (
+    <svg fill="var(--normal-color)" width="62" height="62">
+      <use href={`${sprite}#icon-user`}></use>
+    </svg>
+  );
+
+  const handleAvatarChange = () => {
+    setAvatarUrl('');
+  };
+
   return (
     <Wrapper>
-      <Avatar>
-        <svg fill="var(--normal-color)" width="62" height="62">
-          <use href={`${sprite}#icon-user`}></use>
-        </svg>
-      </Avatar>
-      <Button type="button" onClick={() => alert('Click Button Avatar Add')}>
+      <Avatar>{avatarUrl ? avatarUser : avatarLogo}</Avatar>
+      <Button type="button" onClick={handleAvatarChange}>
         <IconBtn>
           <use href={`${sprite}#icon-add-avatar`}></use>
         </IconBtn>
