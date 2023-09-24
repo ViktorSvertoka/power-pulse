a2f78c26be0db5d86f54f27cb6920cca8552e637
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-09-24 20:13:49 +0300

    Section Radio buttons
    Section Radio buttons

--- src/components/UserProfile/UserProfile.styled.jsx
+++ src/components/UserProfile/UserProfile.styled.jsx
@@ -22,19 +22,14 @@ export const Photo = styled.img`
   border-radius: 100px;
 `;
 
-export const Button = styled.button`
-  position: relative;
-  top: -13px;
-  width: 22px;
-  height: 22px;
-  border: none;
-  background-color: transparent;
+export const Button = styled.div`
+  cursor: pointer;
 `;
 
 export const IconBtn = styled.svg`
   width: 24px;
   height: 24px;
   position: relative;
-  top: -1.5px;
-  left: -7px;
+  top: -13px;
+  left: 0px;
 `;
