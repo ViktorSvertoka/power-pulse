d933334aa77e612e44f87eab0285dbab6b3b2bf3
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-09-26 00:42:41 +0300

    get query to user

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -98,6 +98,27 @@ export const updateUserParams = createAsyncThunk(
   },
 );
 
+export const getUserParams = createAsyncThunk(
+  'auth/getparams',
+  async (userId, thunkAPI) => {
+    // Reading the token from the state via getState()
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      // If there is no token, exit without performing any request
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      const res = await axios.get(`/api/auth/${state.auth.user._id}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
 export const updateAvatar = createAsyncThunk(
   'auth/avatar',
   async (file, thunkAPI) => {
