9ff6eab8b5745a231fc77ce936e38ba196686192
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-10-01 13:29:28 +0200

    Fix (Statistic Cards)

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://power-pulse-back-end.onrender.com';
+axios.defaults.baseURL = 'http://localhost:3000';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
